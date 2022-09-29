5b1633059a7ced65e70de4991abbbc57d4fa4562
Hugo Escalpelo <hugoescalpelo@gmail.com>
2022-09-29 13:47:56 -0500

    Create read-tag.py

---
+++ read-tag.py
@@ -0,0 +1,19 @@
+# Bibliotecas
+from time import sleep
+import sys
+from mfrc522 import SimpleMFRC522
+
+# Inicar el sensor
+reader = SimpleMFRC522()
+
+# Cuerpo del programa
+try:
+    # Leer el tag
+    while True:
+        print("Hold a tag near the reader")
+        id, text = reader.read()
+        print("ID: %s\nText: %s" % (id,text))
+        sleep(5)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    raise
