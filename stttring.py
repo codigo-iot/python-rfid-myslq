a40dce5b1c36d78b3723697374965451acfce215
Hugo Escalpelo <hugoescalpelo@gmail.com>
2023-05-08 12:02:20 -0600

    Update stttring.py

--- stttring.py
+++ stttring.py
@@ -3,6 +3,6 @@ texto = "Texto del RFID"
 rfid = 8462519551
 
 # "INSERT INTO rfid (nombre,texto,rfid) VALUES ('Hugo Vargas','Test Python 4',88843565);"
-query = "INSERT INTO rfid (nombre,texto,rfid) VALUES ('" + nombre + "','" + texto + "'," + str(rfid) + ")"
+query = "INSERT INTO rfid (nombre,texto,ifid_id) VALUES ('" + nombre + "','" + texto + "'," + str(rfid) + ")"
 
 print (query)
